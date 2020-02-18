f6e40ed4efd5344d0102ffe2d9bd6a6d3a22d051
FooBarAUT <39121688+FooBarAUT@users.noreply.github.com>
2020-02-18 21:30:03 +0100

    Change: loop trough URL's

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from src import scrapeTumblr
 from src import fileIO
 
-"""
-https://www.tumblr.com/tagged/hilarious-memes
-https://www.tumblr.com/tagged/funny-memes
-https://www.tumblr.com/tagged/funny-meme
-"""
-URL = 'https://www.tumblr.com/tagged/funny-meme'
-
-list = scrapeTumblr.get_stage_images_from_URL(URL)
-
+urls = ['https://www.tumblr.com/tagged/funny-meme',
+        'https://www.tumblr.com/tagged/funny-memes',
+        'https://www.tumblr.com/tagged/hilarious-memes']
 counter = 1
-for image in list:
-    fileIO.download_image(image, "image/", "test" + str(counter))
-    counter += 1
+
+for url in urls:
+    list = scrapeTumblr.get_stage_images_from_URL(url)
+
+    for image in list:
+        fileIO.download_image(image, "images/", "test" + str(counter))
+        counter += 1
